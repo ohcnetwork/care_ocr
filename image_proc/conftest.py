4d5cbc2ab38f57040c07a4bf8914e2bbd62539a9
Extreme <extreme@pop-os.localdomain>
2022-07-22 21:39:58 +0530

    Initial Template

---
+++ image_proc/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from image_proc.users.models import User
+from image_proc.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
