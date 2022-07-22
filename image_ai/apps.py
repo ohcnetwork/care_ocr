6fb9c1aca0944e34978d655344b29ffd4a6a30f3
Extreme <extreme@pop-os.localdomain>
2022-07-22 22:12:12 +0530

    keras added

--- image_ai/apps.py
+++ image_ai/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+import keras_ocr 
 
 class ImageAiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'image_ai'
+    pipeline = keras_ocr.pipeline.Pipeline()
