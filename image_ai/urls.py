8238684783446c1c2a46615c28ce1b28bf362009
Extreme <extreme@pop-os.localdomain>
2022-07-24 00:52:16 +0530

    predict view added

---
+++ image_ai/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+# from fire_detect.views import UploadPredictView
+
+# urlpatterns = [
+#     path("api/predict", UploadPredictView.as_view(), name="prediction"),
+# ]
--- image_ai/views.py
+++ image_ai/views.py
@@ -1,3 +1,34 @@
+from django.db import DatabaseError
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, JSONParser
+import cloudinary.uploader
+from image_ai.utils import main
+
+
+class UploadPredictView(APIView):
+    parser_classes = (
+        MultiPartParser,
+        JSONParser,
+    )
+
+    @staticmethod
+    def post(request):
+        file = request.data.get("image")
+        upload_data = cloudinary.uploader.upload(file)
+        # print(upload_data)
+        img = upload_data["url"]
+       
+        data = main(img)
+        return Response(
+            {
+                "status": "success",
+                "url": img,
+                "dara": data,
+            },
+            status=201,
+        )
+
 
 # Create your views here.
